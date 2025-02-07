d9b3bcf118abc8882ff5dfcc32bda2bc307f87c3
MizanRbf <mizanrbf@gmail.com>
2025-02-07 08:52:34 +0600

    task-4 done

---
+++ Task-4/array.js
@@ -0,0 +1,7 @@
+const flower = ['rose', 'lily', 'hasnahena'];
+const fruits = ['mango', 'jackfruit'];
+const numbers = 33;
+
+console.log(Array.isArray(flower));
+console.log(Array.isArray(fruits));
+console.log(Array.isArray(numbers));

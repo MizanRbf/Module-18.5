22dfb55791e0fb048abb9d56aa72b91b198fd54c
MizanRbf <mizanrbf@gmail.com>
2025-02-07 08:29:42 +0600

    task-2 done

---
+++ Task-2/array.js
@@ -0,0 +1,10 @@
+const touristSpot = ['coxBazar', 'kuakata', 'sylhet'];
+
+touristSpot.push('panchagarh');
+console.log(touristSpot);
+
+touristSpot.push('bandarban', 'ambagan');
+console.log(touristSpot);
+
+touristSpot.pop();
+console.log(touristSpot);
